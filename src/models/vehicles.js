4685d819bfa14696e401e32423b1e91b6e629e8d
Ramdas Hedgapure <hedgapurer@gmail.com>
2021-11-04 20:28:01 +0530

    vehicle

---
+++ src/models/vehicles.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const users = require('./users')
+
+
+const newsSchema = new mongoose.Schema({
+    title : {type:String, required:true},
+    price : {type:Number, required:true},
+    key_specs : {type:Array, required:true},
+    images : {type:Array, required:true},
+    features : {type:Array, required:true},
+    update : {type:String, required:true},
+},
+{
+    versionKey:false,
+    timestamps:true
+})
+
+module.exports = mongoose.model('news', newsSchema)
